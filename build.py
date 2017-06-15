1fe76dcc9bdf575542d45ef7c280eead230e6fff
rspitler <rspitler@alienvault.com>
2017-06-15 14:47:23 -0500

    initial commit of sync/pull/list

---
+++ build.py
@@ -0,0 +1,34 @@
+from pybuilder.core import init, use_plugin
+
+use_plugin("python.core")
+use_plugin("python.install_dependencies")
+# use_plugin("python.coverage")
+use_plugin("python.distutils")
+use_plugin("exec")
+use_plugin("python.unittest")
+use_plugin("python.pylint")
+use_plugin("copy_resources")
+use_plugin("python.pycharm")
+default_task = "publish"
+
+
+@init
+def initialize(project):
+
+    build_number = project.get_property("build_number")
+    if build_number is not None and "" != build_number:
+        project.version = build_number
+    else:
+        project.version = "0.0.999"
+    #Project Manifest
+    project.summary = "CLI for managing micro-services"
+    project.home_page = "https://github.com/AlienVault-Engineering/service-manager"
+    project.description = "CLI for managing micro-services"
+    project.author = "AlienVault"
+    project.license = "Apache 2.0"
+    project.url = "https://github.com/AlienVault-Engineering/service-manager"
+    project.depends_on_requirements("requirements.txt")
+    #Build and test settings
+    project.set_property("run_unit_tests_propagate_stdout",True)
+    project.set_property("run_unit_tests_propagate_stderr",True)
+    project.set_property("coverage_branch_threshold_warn", 50)
---
+++ src/main/python/commandline.py
@@ -0,0 +1,57 @@
+import argparse
+import logging
+
+from lib.initializer import Initializer
+from lib.log_handler import configure_logging
+from lib.services import load_service_definitions, pretty_print_services
+from lib.vcs import VCS
+
+LIST = 'list'
+SYNC = 'sync'
+PULL = 'pull'
+parser = argparse.ArgumentParser(prog="service-manager", description='Utility to help deploy and manage microservices.')
+
+parser.add_argument('--verbose', help='Level of logging to output', action="store_true", default=False)
+parser.add_argument('--application-filter', help='Constrain to passed application', type=str, default=None)
+parser.add_argument('--vcs-user', help='Username for VCS', type=str, default=None, required=True)
+parser.add_argument('--vcs-password', help='Password for VCS', type=str, default=None, required=True)
+parser.add_argument('--repo-root', help='Root user for BitBucket', type=str, default='alienadmin')
+parser.add_argument('--service-directory', help='Directory containing service definitions in <app>/service.json format', type=str, default='../../../../services')
+parser.add_argument('--dry-run',help='Preview effect of action', action="store_true",
+                         default=False)
+subparsers = parser.add_subparsers(help='commands')
+
+list_parser = subparsers.add_parser(LIST, help='List known services')
+list_parser.set_defaults(command=LIST)
+list_parser.add_argument('--validate-repository', help='Validate existence of repository in VCS', action="store_true",
+                         default=False)
+sync_parser = subparsers.add_parser(SYNC, help='Sync service definitions and initialize new entries.')
+sync_parser.set_defaults(command=SYNC)
+sync_parser.add_argument('--destination-directory',help='Destination directory to create new service definition.',required=True)
+
+pull_parser = subparsers.add_parser(PULL, help='Pull git repos into local filesystem.')
+pull_parser.set_defaults(command=PULL)
+pull_parser.add_argument('--destination-directory',help='Destination directory to pull code into.',required=True)
+
+
+def execute_main():
+    args = parser.parse_args()
+    configure_logging(args.verbose)
+    application_map = load_service_definitions(args.application_filter, args.service_directory)
+    vcs = VCS(args.vcs_user, args.vcs_password, args.repo_root, args.dry_run)
+    if args.command == LIST:
+        if args.validate_repository:
+            vcs.validate_repositories(application_map)
+        pretty_print_services(application_map)
+    elif args.command == SYNC:
+        vcs.validate_repositories(application_map)
+        init = Initializer(vcs, args.destination_directory, args.dry_run)
+        init.initialize_services(application_map)
+    elif args.command == PULL:
+        vcs.validate_repositories(application_map)
+        vcs.pull_services(application_map, args.destination_directory)
+
+
+if __name__ == "__main__":
+    execute_main()
+
