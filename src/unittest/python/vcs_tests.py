bd6a7f48215aefcb5fc8aa8e9c4e53127313f77c
rspitler <rspitler@alienvault.com>
2018-03-31 12:34:32 -0500

    update API usage

--- src/unittest/python/vcs_tests.py
+++ src/unittest/python/vcs_tests.py
@@ -2,7 +2,7 @@ import os
 
 from service_buddy.service import loader
 from service_buddy.vcs.Bitbucket import BitbucketVCSProvider
-from service_buddy.vcs.vcs import VCS
+from service_buddy.vcs.vcs import VCS, vcs_provider_map
 from testcase_parent import ParentTestCase
 
 DIRNAME = os.path.dirname(os.path.abspath(__file__))
@@ -26,7 +26,7 @@ class VCSTestCase(ParentTestCase):
             self.assertTrue("vcs-config.json" in E.message)
         vcs = VCS(self.service_directory,True)
         self.assertTrue(vcs.default_provider == "bitbucket","Failed to load default provider")
-        self.assertTrue(type(vcs.vcs_providers[vcs.default_provider]) == BitbucketVCSProvider,"Failed to load default provider")
+        self.assertTrue(type(vcs_provider_map[vcs.default_provider]) == BitbucketVCSProvider,"Failed to load default provider")
         application_map = loader.load_service_definitions(self.service_directory)
         vcs.validate_repositories(application_map=application_map)
         vcs.clone_service(application_map=application_map, destination_directory="/tmp/out")
