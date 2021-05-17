8569a4dfbd3b228f8adc8860f1ed440b7dc8d53c
rspitler <rspitler@gmail.com>
2021-05-17 14:20:16 -0600

    update logic for updaing build file

--- src/main/python/service_buddy_too/ci/bitbucket_pipeline_build_creator.py
+++ src/main/python/service_buddy_too/ci/bitbucket_pipeline_build_creator.py
@@ -21,4 +21,7 @@ class BitBucketPipelineBuildCreator(FileBasedBuildCreator):
         return os.path.join(service_dir, "bitbucket-pipelines.yml")
 
     def _build_exists_action(self, service_dir: str, build_template: dict, service_definition: Service):
+        # Bash file so cookie cutter doesnt puke
+        os.remove(self._get_build_file(service_dir))
+        # Rebuild
         super(BitBucketPipelineBuildCreator, self).create_build(service_dir, build_template, service_definition)
