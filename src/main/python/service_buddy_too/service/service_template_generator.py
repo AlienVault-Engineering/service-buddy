bd177f87c13d10baa8b10e399cb8f94a3656c188
rspitler <rspitler@gmail.com>
2021-05-20 13:05:06 -0600

    update to propagate service-defaults into service.json

--- src/main/python/service_buddy_too/service/service_template_generator.py
+++ src/main/python/service_buddy_too/service/service_template_generator.py
@@ -1,6 +1,9 @@
+import json
 import os
 
 # noinspection PyUnresolvedReferences
+import tempfile
+
 from infra_buddy.commandline import cli
 from infra_buddy.commands.generate_service_definition import command as gen_service
 from infra_buddy.context.deploy_ctx import DeployContext
@@ -18,7 +21,16 @@ class ServiceTemplateGenerator(object):
         gen_service.do_command(DeployContext.create_deploy_context(
             application=service_definition.get_app(),
             role=service_definition.get_role(),
-            environment='dev',
-            defaults=defaults
+            environment='dev'
         ), service_type=service_type,
             destination=service_def_folder)
+        if defaults:
+            service_file_path = os.path.join(service_def_folder, 'service.json')
+            with open(service_file_path, 'r') as fp:
+                service_json = json.load(fp=fp)
+                parameters_ = service_json['deployment-parameters']
+                service_json['deployment-parameters'] = {**parameters_ , **defaults}
+            with open(service_file_path, 'w') as fp:
+                json.dump(service_json, fp)
+
+
--- src/unittest/python/initializer_tests.py
+++ src/unittest/python/initializer_tests.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import testcase_parent
@@ -21,7 +22,11 @@ class InitTestCase(ParentTestCase):
     def validate_service_generation(self, definition):
         base_path = os.path.join(self.temp_dir, definition.get_app(), definition.get_fully_qualified_service_name())
         self.assertTrue(os.path.exists(os.path.join(base_path, "service/README.md")), "Did not find readme file")
-        self.assertTrue(os.path.exists(os.path.join(base_path, "service/service.json")), "Did not find service file")
+        servicejson = os.path.join(base_path, "service/service.json")
+        self.assertTrue(os.path.exists(servicejson), "Did not find service file")
+        with open(servicejson) as fp:
+            service = json.load(fp=fp)
+            self.assertTrue('USE_FARGATE' in service['deployment-parameters'], "Failed to find defaults")
 
     def validate_generation(self, definition:Service):
         base_path = os.path.join(self.temp_dir, definition.get_app(), definition.get_fully_qualified_service_name())
@@ -48,9 +53,10 @@ class InitTestCase(ParentTestCase):
         loader.walk_service_map(application_map, application_callback=None, service_callback=self.validate_generation)
 
     def test_service_definition_generation(self):
-        service_directory = os.path.join(testcase_parent.DIRNAME, '../resources/initializer_test')
-        application_map = loader.load_service_definitions(service_directory,code_directory=self.temp_dir)
-        init = Initializer(self.vcs, self.temp_dir, self.service_templates_test,skip_git_creation=True)
+        init_test_dir = os.path.join(testcase_parent.DIRNAME, '../resources/initializer_test')
+        application_map = loader.load_service_definitions(init_test_dir,code_directory=self.temp_dir)
+        init = Initializer(vcs=self.vcs, destination_directory=self.temp_dir,
+                           code_template_directory=init_test_dir,skip_git_creation=True)
         init.initialize_services(application_map)
         loader.walk_service_map(application_map, application_callback=None,
                                 service_callback=self.validate_service_generation)
