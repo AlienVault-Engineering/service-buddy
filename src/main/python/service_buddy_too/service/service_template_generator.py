237a7fbdb4b0e99cac6e97fd189bfd3332a1da4b
rspitler <rspitler@gmail.com>
2021-07-13 16:05:22 -0400

    make sure remote template locations get propagated

--- src/main/python/service_buddy_too/service/service_template_generator.py
+++ src/main/python/service_buddy_too/service/service_template_generator.py
@@ -25,14 +25,15 @@ class ServiceTemplateGenerator(object):
             context.load_remote_defaults(remote_template_locations)
         gen_service.do_command(context, service_type=service_type,
                                destination=service_def_folder)
-        if defaults:
+        if defaults or remote_template_locations:
             service_file_path = os.path.join(service_def_folder, 'service.json')
             with open(service_file_path, 'r') as fp:
                 service_json = json.load(fp=fp)
                 parameters_ = service_json['deployment-parameters']
                 if remote_template_locations:
                     service_json["service-template-definition-locations"] = remote_template_locations
-                service_json['deployment-parameters'] = {**parameters_, **defaults}
+                if defaults:
+                    service_json['deployment-parameters'] = {**parameters_, **defaults}
             with open(service_file_path, 'w') as fp:
                 json.dump(service_json, fp, indent=1)
         if ib_defaults:
